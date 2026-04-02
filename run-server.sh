a661ad2b4e451ce69a7c63129e062827a8a6f0a4
bivorzk <bzkugli@gmail.com>
2026-04-02 20:06:45 +0200

    modified run-server

--- run-server.sh
+++ run-server.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
-PUBLIC_SRC="target/dx/snap-tray-auth/release/web/public"
-PUBLIC_DST="target/release/public"
-if [ ! -d "$PUBLIC_DST" ]; then
-    cp -r "$PUBLIC_SRC" "$PUBLIC_DST"
-fi
 export IP=0.0.0.0
-exec ./target/release/snap-tray-auth
+cd target/dx/snap-tray-auth/release/web
+exec ./snap-tray-auth
